8095e2e089dd6dcb4e6b4022ad63ccaa980cae7c
Keymor <vlad.kutuzov96@gmail.com>
2025-04-03 20:52:44 +0700

    server response

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
 const app = express()
 
-app.use(cors())
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
+app.get('/home', (req, res) => {
+    res.send(`<h1>${__dirname}, ${__filename}</h1>`)
+})
+
 app.post('/submit', (req, res) => {
-    console.log(req.body); // Access form data here
-    res.send(`<h1>Received: ${req.body.name}, ${req.body.email}<h1/>`);
+    console.log(req.body);
+    const {serverM} = req.body
+    req.body.serverM = 'Server response: '
+    res.json(req.body);
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
--- src/component/dev.tsx
+++ src/component/dev.tsx
@@ -1,9 +1,42 @@
+import { useState } from "react"
+
 export default function Dev() {
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState([''])
+
+    const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const formData = new URLSearchParams()
+
+        formData.append('name', name)
+
+        try {
+            const response = await fetch('http://localhost:3000/submit', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: formData.toString(),
+            })
+
+            const data = await response.json()
+            console.log(data)
+            setMessage((message) => ([...message, ` ${data.serverM} `, `${data.name} `]))
+        } catch (error) {
+            console.error(error)
+        }
+
+    }
+
     return (
-        <form action="/submit" method="POST" className="mx-auto mb-10 bg-pink-200 p-4 rounded-2xl flex flex-row gap-4">
-            <input type="text" name="name" placeholder="Your Name" className="bg-pink-50 p-2 rounded-md"/>
-            <input type="email" name="email" placeholder="Your Email" className="bg-pink-50 p-2 rounded-md"/>
-            <button type="submit" className="bg-pink-300 rounded-md p-2 cursor-pointer">Send</button>
-        </form>
+        <div>
+            <form onSubmit={submitForm} className="mx-auto mb-10 bg-pink-200 p-4 rounded-2xl flex flex-row gap-4">
+                <input onChange={(e) => { setName(e.target.value) }} value={name} type="text" name="name" placeholder="Your Name" className="bg-pink-50 p-2 rounded-md" />
+                {/* <input type="email" name="email" placeholder="Your Email" className="bg-pink-50 p-2 rounded-md" /> */}
+                <button type="submit" className="bg-pink-300 rounded-md p-2 cursor-pointer">Send</button>
+                <div style={{visibility: message.length < 3 ? 'hidden' : 'visible'}} className="my-auto bg-pink-100 p-2 rounded-md">{message}</div>
+            </form>
+        </div>
     )
 }
